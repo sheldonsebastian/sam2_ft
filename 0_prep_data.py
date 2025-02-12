ee84a0ed743b4e547e02d02bc4821dac7e3c82dc
Sheldon Sebastian <51760617+sheldonsebastian@users.noreply.github.com>
2025-02-12 14:27:04 -0500

    training_sav_png_fixed seems to be working

--- 0_prep_data.py
+++ 0_prep_data.py
@@ -2,7 +2,6 @@
 import cv2
 import os
 import json
-import numpy as np
 from pycocotools import mask as maskUtils
 
 
@@ -53,8 +52,7 @@ def save_mask_as_png(mask, output_path):
     cv2.imwrite(output_path, mask)
 
 
-def create_combined_masks(data, output_folder):
-    combined_masks = {}
+def create_object_masks(data, output_folder):
 
     for frame_num, masklets_arr in enumerate(data["masklet"]):
         for obj_id, mask_data in enumerate(masklets_arr):
@@ -62,21 +60,11 @@ def create_combined_masks(data, output_folder):
             rle = {"counts": mask_data["counts"], "size": [height, width]}
             binary_mask = rle_to_mask(rle, height, width)
 
-            # Initialize the combined mask for the frame if not already done
-            if frame_num not in combined_masks:
-                combined_masks[frame_num] = np.zeros((height, width, 3), dtype=np.uint8)
+            object_folder = os.path.join(output_folder, str(obj_id).zfill(5))
+            os.makedirs(object_folder, exist_ok=True)
 
-            # Assign a unique color to each object in the mask
-            color = (obj_id * 50 % 256, obj_id * 100 % 256, obj_id * 150 % 256)
-            combined_masks[frame_num][binary_mask > 0] = color
-
-    # Save the combined masks for each frame
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    for frame_num, combined_mask in combined_masks.items():
-        output_path = os.path.join(output_folder, f"{str(frame_num).zfill(5)}.png")
-        save_mask_as_png(combined_mask, output_path)
+            output_path = os.path.join(object_folder, f"{str(frame_num).zfill(5)}.png")
+            save_mask_as_png(binary_mask, output_path)
 
 
 # %% EXTRACT FRAMES
@@ -93,7 +81,7 @@ for i, video_file in enumerate(video_files, start=1):
             r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\raw_mini-dataset", video_file
         ),
         os.path.join(
-            r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png",
+            r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png_fixed",
             "images",
             f"video_{i:04d}",
         ),
@@ -119,14 +107,36 @@ for i, video_mask_json_path in enumerate(video_mask_json_files, start=1):
         data = json.load(f)
 
     output_folder = os.path.join(
-        r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png",
+        r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png_fixed",
         "annotations",
         f"video_{i:04d}",
     )
-    create_combined_masks(data, output_folder)
+    create_object_masks(data, output_folder)
 
 
 # %%
 print("Finito")
+
+# %% Quick Validation
+# import matplotlib.pyplot as plt
+# from skimage.io import imread
+# import numpy as np
+
+# # %%
+# for filename in sorted(
+#     os.listdir(
+#         r"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png_fixed\annotations\video_0001\00004"
+#     )
+# ):
+#     print(filename)
+
+#     x = imread(
+#         rf"C:\Users\sheld\OneDrive\Workspaces\sam2_ft\prepped_mini_dataset_png_fixed\annotations\video_0001\00004\{filename}"
+#     )
+
+#     print(np.unique(x))
+
+#     plt.imshow(x)
+#     plt.show()
 
 # %%
